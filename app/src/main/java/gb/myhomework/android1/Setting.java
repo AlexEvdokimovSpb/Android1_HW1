6e8db05c7c932aa1f94e682ffa6ef8de142e88d9
AlexEvdokimovSpb <3416216@gmail.com>
2020-10-29 02:48:05 +0300

    HW2

---
+++ app/src/main/java/gb/myhomework/android1/Setting.java
@@ -0,0 +1,13 @@
+package gb.myhomework.android1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Setting extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting);
+    }
+}
