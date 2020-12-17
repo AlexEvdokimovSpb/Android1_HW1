6b7000f5e1aee7951d38eb0456c54595e41fe5f5
AlexEvdokimovSpb <3416216@gmail.com>
2020-12-17 05:30:38 +0300

    2_HW3 Что сделано: MyIntentService extends IntentService Теперь данные о погоде из MainFragment у сервера погоды запрашиваются через данный сервис

--- app/src/main/java/gb/myhomework/android1/Constants.java
+++ app/src/main/java/gb/myhomework/android1/Constants.java
@@ -3,6 +3,7 @@ package gb.myhomework.android1;
 public  class Constants {
     static String  SETTING = "SETTING";
     static String  PUBLISHER = "PUBLISHER";
+    static String  TO_SERVIS = "TO_SERVIS";
     static final int REQUEST_CODE =33;
     public static boolean VERBOSE = true;
     public static boolean DEBUG= VERBOSE&&true;
